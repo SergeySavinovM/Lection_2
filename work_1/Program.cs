b42e88772dea677081eb03a024490f1eb2bc58c4
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-09 12:52:07 +0300

    Создал папку с первой лекционной задачей

---
+++ work_1/Program.cs
@@ -0,0 +1,21 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result ) result = arg2;
+    if(arg3 > result ) result = arg3;
+    return result;
+}
+
+int num1 = 13;
+int num2 = 25;
+int num3 = 9;
+int num4 = 73;
+int num5 = 16;
+int num6 = 27;
+int num7 = 3;
+int num8 = 74;
+int num9 = 30;
+
+int max = Max(Max(num1, num2, num3),Max(num4, num5, num6),Max(num7, num8, num9));
+
+Console.WriteLine(max);
