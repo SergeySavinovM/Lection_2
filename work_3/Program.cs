002a7acc1c295fd009bf1cd993ecbfdf65fba979
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-11 10:01:35 +0300

    Создал папку с поиском индекса нужного числа

---
+++ work_3/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {12, 6, 76, 45, 91, 23, 45};
+
+int n = array.Length;
+int find = 23;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
